bc9929ff2552ec6b7a677230ca9a09654d0e4926
Patryk Krzywda <patryk.krzywda32@gmail.com>
2023-04-13 16:27:26 +0200

    V1.1

--- Zestaw 3/Zad4.cpp
+++ Zestaw 3/Zad4.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
 int NWD(int a, int b){
     while(a != b){
         if(a > b){
@@ -14,6 +15,7 @@ int NWD(int a, int b){
     }
     return a;
 }
+
 int wykladnik(int a, int  b){
     int d = 1;
     while((d * a) % b != 1){
@@ -21,10 +23,12 @@ int wykladnik(int a, int  b){
     }
     return d;
 }
+
 int szyfrowanie(int n, int e, int liczba){
     int c = fmod(pow(liczba,e),n);
     return c;
 }
+
 int odszyfrowanie(int n, int d, int liczba){
     int wynik = 1;
     for(d; d > 0; d /= 2 )
@@ -34,17 +38,20 @@ int odszyfrowanie(int n, int d, int liczba){
     }
     return wynik;
 }
+
 int main(){
 
     int p = 53, q = 71, liczba, d, wybor;
     int delta = (p-1) * (q-1);
     int n = p*q;
     int e = 2;
+
     do{
         cout << "1. Szyfrowanie liczby" << endl;
         cout << "2. Odszyfrowanie liczby" << endl;
         cin >> wybor;
     }while(wybor != 1 && wybor != 2);
+
     if(wybor == 1){
         cout << "Podaj liczbę do zaszyfrowania: " << endl;
         cin >> liczba;
@@ -52,7 +59,8 @@ int main(){
         d = wykladnik(e, delta);
         liczba = szyfrowanie(n, e, liczba);
         cout << "Zaszyfrowana liczba: " << liczba << endl;
-    }else{
+    }
+    else{
         cout << "Podaj liczbę do zaszyfrowania: " << endl;
         cin >> liczba;
         while(NWD(e,delta)!=1) e++;
@@ -60,6 +68,5 @@ int main(){
         liczba = odszyfrowanie(n,d,liczba);
         cout << "Odszyfrowana liczba: " << liczba << endl;
     }
-    
     return 0;
 }
